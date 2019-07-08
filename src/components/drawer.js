2148610680dab5b0a883dad43426272227269b5b
ivan <edu.zvanoz@gmail.com>
2019-07-08 14:23:30 +0300

    Урок 5. SVG иконки Самописные SVG-иконки в коде, а также импорт готовых SVG-иконок из пакета "material-ui".

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import {SvgIcon} from "material-ui";
+
+// Урок5. IconContacts и IconContactMail - это SVG-иконки, данные для отрисовки которых импортируются из "material-ui".
+import IconContacts from "material-ui/svg-icons/communication/contacts"
+import IconContactMail from "material-ui/svg-icons/communication/contact-mail"
+import {blue700, pink700, yellow500} from "material-ui/styles/colors";
+
+// Урок5. HomeIcon - это SVG-иконка, данные для отрисовки которой вшиты в код.
+const HomeIcon = (props) => (
+	<SvgIcon {...props}>
+		<path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>
+	</SvgIcon>
+);
 
 export class AppDrawer extends Component {
 	// constructor(props) {
 	// 	super(props);
 	// 	// this.props = props;
 	// }
-
 	render() {
 		return (
 			<Drawer
@@ -15,8 +27,30 @@ export class AppDrawer extends Component {
 				open={this.props.open}
 				onRequestChange={this.props.onRequestChange}
 			>
-				<MenuItem>Главная</MenuItem>
-				<MenuItem>Контакты</MenuItem>
+				<MenuItem
+					leftIcon={
+						<HomeIcon
+							color={blue700}
+							hoverColor={pink700}
+						/>
+					}
+				>Главная</MenuItem>
+				<MenuItem
+					leftIcon={
+						<IconContacts
+							color={blue700}
+							hoverColor={pink700}
+						/>
+					}
+				>Контакты</MenuItem>
+				<MenuItem
+					leftIcon={
+						<IconContactMail
+							color={blue700}
+							hoverColor={pink700}
+						/>
+					}
+				>Электронная почта</MenuItem>
 			</Drawer>
 		);
 	}
